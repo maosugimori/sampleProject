c6dcb6b65ad62b81ab820570e9d6b4dfb98ed0a0
杉森 磨褒 <test@MAOSUGIMORI.local>
2026-02-06 16:23:13 +0900

    ページ表示練習

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,14 @@
 import { LinkComponent } from "../components/LinkComponent";
+import PostForm from "../components/PostForm";
 
 export default function About() {
   return (
       <div> 
           <h1>Aboutな ページ</h1>
+
+          <div className="">
+            <PostForm />
+          </div>
           hogeコンポーネントのリンクはこちら→ <LinkComponent linkPath="/contact" linkName="contactページへ" />
       </div>
   )
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,14 @@
 import { LinkComponent } from "../components/LinkComponent";
+import PhotoUpload from "../components/Common/photoUpload";
+import Ca4laSite from "../components/ca4laSite";
 
 export default function Contact() {
   return (
       <div> 
           <h1>Contact ページ</h1>
           hogeコンポーネントのリンクはこちら→ <LinkComponent linkPath="/about" linkName="aboutページへ" />
+
+          <Ca4laSite />
       </div>
   )
 
